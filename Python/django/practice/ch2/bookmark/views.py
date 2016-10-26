1ce56b5da2b43fb73ca17dd506dd9aa5f609e046
Seoksoon Jang <boraseoksoon@gmail.com>
2016-10-26 17:56:19 +0900

    ADD django bookmark test app
    ADD django bookmark test app

---
+++ Python/django/practice/ch2/bookmark/views.py
@@ -0,0 +1,18 @@
+# no need now
+from django.shortcuts import render
+
+
+# ---- user import
+from django.views.generic import ListView, DetailView
+from bookmark.models import Bookmark
+
+# Create your views here.
+
+# ---- ListView
+class BookmarkLV(ListView) : 
+	model = Bookmark
+
+# ---- DetailView
+class BookmarkDV(DetailView) : 
+	model = Bookmark
+
