0139cf3bcc6f0eec190e1f40a5dfbd3eeb0bafa7
Seoksoon Jang <boraseoksoon@gmail.com>
2017-03-10 01:18:55 +0900

    ADD few way to draw
    just fun coding studying core graphic again since it's been a while.

--- Swift/DailyPractice/20170309.playground/Contents.swift
+++ Swift/DailyPractice/20170309.playground/Contents.swift
@@ -2,9 +2,91 @@
 
 import UIKit
 
+
 var str = "Swift Daily Coding : 20170309"
 
-// draw a required triangle given one side
+// function base
+func drawRectangle(rectangleFrame: CGRect, backgroundColor: UIColor) -> UIView {
+    let rectangle = UIView(frame:rectangleFrame)
+    rectangle.backgroundColor = backgroundColor
+    
+    return rectangle
+}
+
+let simpleRectangle = drawRectangle(rectangleFrame:CGRect(x:0, y:0, width: 200, height: 200),
+                                    backgroundColor: UIColor.red)
+
+func drawCircle(circleFrame: CGRect, backgroundColor: UIColor) -> UIView {
+    let circle = UIView(frame:circleFrame)
+    circle.backgroundColor = backgroundColor
+    
+    circle.layer.masksToBounds = true
+    circle.layer.cornerRadius = 25
+    
+    return circle
+}
+
+let simpleCircle = drawCircle(circleFrame: CGRect(x: 0, y: 0, width: 50, height: 50),
+                              backgroundColor: UIColor.yellow)
+
+
+class CrossButton: UIButton {
+    var fillColor: UIColor = UIColor.blue
+    var isAddButton: Bool = true
+    
+    override func draw(_ rect: CGRect) {
+        
+        let path = UIBezierPath(ovalIn: rect)
+        fillColor.setFill()
+        path.fill()
+        
+        //set up the width and height variables
+        //for the horizontal stroke
+        let plusHeight: CGFloat = 3.0
+        let plusWidth: CGFloat = min(bounds.width, bounds.height) * 0.6
+        
+        //create the path
+        let plusPath = UIBezierPath()
+        
+        //set the path's line width to the height of the stroke
+        plusPath.lineWidth = plusHeight
+        
+        //move the initial point of the path
+        //to the start of the horizontal stroke
+        plusPath.move(to: CGPoint(
+            x:bounds.width/2 - plusWidth/2 + 0.5,
+            y:bounds.height/2 + 0.5))
+        
+        //add a point to the path at the end of the stroke
+        plusPath.addLine(to: CGPoint(
+            x:bounds.width/2 + plusWidth/2 + 0.5,
+            y:bounds.height/2 + 0.5))
+        
+        //Vertical Line
+        if isAddButton {
+            //move to the start of the vertical stroke
+            plusPath.move(to: CGPoint(
+                x:bounds.width/2 + 0.5,
+                y:bounds.height/2 - plusWidth/2 + 0.5))
+            
+            //add the end point to the vertical stroke
+            plusPath.addLine(to: CGPoint(
+                x:bounds.width/2 + 0.5,
+                y:bounds.height/2 + plusWidth/2 + 0.5))
+        }
+        
+        //set the stroke color
+        UIColor.white.setStroke()
+        
+        //draw the stroke
+        plusPath.stroke()
+        
+    }
+}
+
+let customButton = CrossButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+
+// draw a required triangle given one side using CoreGraphics on Class
 class Triangle : UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -34,3 +116,6 @@ class Triangle : UIView {
 let triangleFrame = CGRect(x: 10, y: 20, width: 25 , height: 30)
 let triangle = Triangle(frame: triangleFrame)
 triangle.backgroundColor = .white
+
+
+
