9f1daf544e0e3a09313cdf7e3c7e54c11ccfba76
Seoksoon Jang <boraseoksoon@gmail.com>
2017-09-04 01:58:18 +0900

    [FIX] go bubble sort in swift
    bubble bubble

--- Swift/DailyPractice/NSquareSortings.playground/Contents.swift
+++ Swift/DailyPractice/NSquareSortings.playground/Contents.swift
@@ -14,15 +14,30 @@ func swap<T>(target: inout T, source: inout T) -> Void {
   source = temp
 }
 
-var inputArray = Array(0..<100).map({ transform  in
-  return Int(arc4random_uniform(1000 + 1))
-})
+var inputArray = Array(0..<30).map({ transform  in return Int(arc4random_uniform(1000 + 1)) })
 
 // insertion sort
 
-
 // bubble sort
-
+func bubbleSort<T: Comparable>(inputArray: [T]) throws -> [T] {
+  guard !inputArray.isEmpty else {
+    throw SortError.EmptyInputArray(msg: "input array is empty!")
+  }
+  
+  var arrayToSort = inputArray
+  for (index, _) in arrayToSort.enumerated() {
+    for subIndex in 0 ..< arrayToSort.count - index {
+      guard subIndex < arrayToSort.count - 1 else {
+        continue
+      }
+      if arrayToSort[subIndex] > arrayToSort[subIndex+1] {
+        swap(target: &arrayToSort[subIndex+1], source: &arrayToSort[subIndex])
+      }
+    }
+  }
+  let sortedArray = arrayToSort
+  return sortedArray
+}
 
 // selection sort
 func selectionSort<T: Comparable>(inputArray: [T]) throws -> [T] {
@@ -45,3 +60,4 @@ func selectionSort<T: Comparable>(inputArray: [T]) throws -> [T] {
 }
 
 let sortedArray = try! selectionSort(inputArray: inputArray)
+let sortedArray2 = try! bubbleSort(inputArray: inputArray)
