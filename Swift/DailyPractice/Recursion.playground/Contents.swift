9b0ba183e50aba7c52685a4f412599cf6ca52110
Seoksoon Jang <boraseoksoon@gmail.com>
2017-09-10 04:46:21 +0900

    [ADD] tail recursion and related problem solving study for fun

--- Swift/DailyPractice/Recursion.playground/Contents.swift
+++ Swift/DailyPractice/Recursion.playground/Contents.swift
@@ -33,6 +33,83 @@ let result2 = tailFactorial(initValue: 1, count: 5)
 print(result2)
 
 
+
+/// summation
+
+// usual
+func summation(_ n: Int) -> Int {
+  if n < 1 {
+    return 0
+  }
+  return n + summation(n - 1)
+}
+
+let result3 = summation(100)
+print(result3)
+
+// tail call
+func tailSummation(number: Int, initValue: Int = 0) -> Int {
+  if number < 1 {
+    return initValue
+  }
+  return tailSummation(number: number-1,
+                       initValue: initValue + number)
+}
+
+let result4 = tailSummation(number: 100, initValue: 0)
+print(result4)
+
+
+/// fibonnaci
+let fiboLevel = 5
+// 1,1,2,3,5,8,13,21.....
+func fibo(level: Int) -> Int {
+  guard level > 1 else { return level }
+  return fibo(level: level-1) + fibo(level: level-2)
+}
+
+let result6 = fibo(level: fiboLevel)
+print("result6 :", result6)
+
+func tailFibo(level: Int, previous: (Int, Int) = (0,1)) -> Int {
+  guard level > 0 else { return 0 }
+  if level == 1 {
+    return previous.1
+  }
+  return tailFibo(level: level - 1,
+                  previous: (previous.1, previous.0 + previous.1))
+}
+
+let result7 = tailFibo(level: fiboLevel)
+print("result7 :", result7)
+
+/// trampoline
+enum Result<T> {
+  case Done(T)
+  case Call(() -> Result<T>)
+}
+
+func sum3(n: Int, acc: Int = 0) -> Int {
+  func helper(_ n: Int, acc: Int) -> Result<Int> {
+    if n < 1 {
+      return .Done(acc)
+    }
+    return .Call({ helper(n-1, acc: n + acc) })
+  }
+  
+  var res = helper(n, acc: acc)
+  repeat {
+    switch res {
+      case let .Done(r): return r
+      case let .Call(f): res = f()
+    }
+  } while true
+}
+
+let result5 = sum3(n: 100)
+print("result5 :", result5)
+
+/// recursive closure
 ///
 func recursiveClosure(_ closure: @escaping (()->Void)->Void) -> ()->Void {
   return { closure(recursiveClosure(closure)) }
